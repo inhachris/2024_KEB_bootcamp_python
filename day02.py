7ff4d28781b9d32ad0d315a90c8787a869b9d095
chris <chris4364@inha.edu>
2024-01-15 13:34:23 +0900

    v0.8 python day02

--- day02.py
+++ day02.py
@@ -1,21 +1,12 @@
-base_number = int(input('Input base number : '))
-exponent_number = int(input('Input exponent number : '))
-print(type(base_number), type(exponent_number))
+first_number = int(input("First number : "))
+second_number = int(input("Second number : "))
 
-# f-string
-# print(f'밑은 {base_number}, 지수는 {exponent_number}, 결과 값은 {base_number ** exponent_number}')
-print(f'밑은 {base_number}, 지수는 {exponent_number}, 결과 값은 {pow(base_number, exponent_number)}')
+print(f'몫은 {divmod(first_number, second_number)[0]} 나머지는 {divmod(first_number, second_number)[1]}입니다.')
 
-# format function
-print('밑은 {0}, 지수는 {1}, 결과 값은 {2}'.format(base_number, exponent_number, pow(base_number,exponent_number)))
-print('밑은 {}, 지수는 {}, 결과 값은 {}'.format(base_number, exponent_number, pow(base_number,exponent_number)))
-
-# C like
-print('밑은 %d, 지수는 %d, 결과 값은 %d' % (base_number, exponent_number, pow(base_number, exponent_number)))
-
-# money = 5,000,000
-# print(money)
-# print(type(money)) # tuple
-# cash = 5_000_000
-# print(cash)
-# print(type(cash)) # int
+# first_number = int(input("First number : "))
+# second_number = int(input("Second number : "))
+#
+# quotient = first_number // second_number
+# remainder = first_number % second_number
+#
+# print(f'몫은 {quotient} 나머지는 {remainder}입니다.')
