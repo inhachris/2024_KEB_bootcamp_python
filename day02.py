e845a04bb978a368fac4aaf25716f67e1fb41016
chris <chris4364@inha.edu>
2024-01-15 15:48:01 +0900

    v1.3 python day02

--- day02.py
+++ day02.py
@@ -1,20 +1,9 @@
-# (100°F - 32) × 5/9 = 37.778°C
-# (0°C × 9/5) + 32 = 32°F
+letter = input("Input alphabet letter : ")
+# vowels = {'a', 'e', 'i', 'o', 'u'}     # set
+vowels = "aeiou"     # str
+print(type(vowels))
 
-menu = input("1) Fahrenheit -> Celsius     2) Celsius -> Fahrenheit     3) Quit Program : ")
-
-if menu == '1':
-    fahrenheit = float(input('Input Fahrenheit : '))
-    print(f'Fahrenheit : {fahrenheit}°F, Celsius : {((fahrenheit - 32.0) * 5.0 / 9.0):.4f}°C')
-elif menu == '2':
-    celsius = float(input('Input Celcius : '))
-    print(f'Celsius : {celsius}°C, Fahrenheit : {((celsius*9.0/5.0) + 32.0):.4f}°F')
+if letter in vowels:     # in
+    print(f'{letter} is a vowel~')
 else:
-    print('Terminate Program')
-
-# temp = [0]
-# temp = []
-# if temp:
-#     print("원소가 존재 하는 리스트")
-# else:
-#     print("비어 있는 리스트")
+    print(f'{letter} is a consonant!')
