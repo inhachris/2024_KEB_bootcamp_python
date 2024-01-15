7113dc9316017db34565c7d23ea49751135288f9
chris <chris4364@inha.edu>
2024-01-15 10:55:43 +0900

    v0.4 python day02

--- day02.py
+++ day02.py
@@ -1,11 +1,17 @@
-univ = 'Inha university'
-print(univ)
-print(univ[5])
-# univ[5] = 'U'  # immutable
-# print(univ[5])
+# SyntaxError: cannot assign to literal here.
+# "univ" = "Inha University"
+# 99 =71
 
-subjects = ['Python', 'C++', 'Linux', 'Data Structure', 'Database']
-print(subjects)
-print(subjects[3])
-subjects[3] = 'Data Structure & Algorithm' # mutable
-print(subjects)
+# Ok
+# case-sensitive
+abc = 7
+Abc = 8
+ABC = 6
+print(abc, ABC, Abc)
+
+test9 = 77
+# 9test = 77
+_9test = 71
+print(test9, _9test)
+
+# False = 123 # reserved word
