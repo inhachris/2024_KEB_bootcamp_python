2ed50219c3856474df134c21c48b0098c1e4d41e
chris <chris4364@inha.edu>
2024-01-15 10:45:04 +0900

    v0.3 python day02

---
+++ day02.py
@@ -0,0 +1,11 @@
+univ = 'Inha university'
+print(univ)
+print(univ[5])
+# univ[5] = 'U'  # immutable
+# print(univ[5])
+
+subjects = ['Python', 'C++', 'Linux', 'Data Structure', 'Database']
+print(subjects)
+print(subjects[3])
+subjects[3] = 'Data Structure & Algorithm' # mutable
+print(subjects)
