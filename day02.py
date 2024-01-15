e00ddf9e97841eed84ca0d2d278a7afd41d0d3aa
chris <chris4364@inha.edu>
2024-01-15 15:42:57 +0900

    v1.2 python day02

--- day02.py
+++ day02.py
@@ -9,3 +9,12 @@ if menu == '1':
 elif menu == '2':
     celsius = float(input('Input Celcius : '))
     print(f'Celsius : {celsius}°C, Fahrenheit : {((celsius*9.0/5.0) + 32.0):.4f}°F')
+else:
+    print('Terminate Program')
+
+# temp = [0]
+# temp = []
+# if temp:
+#     print("원소가 존재 하는 리스트")
+# else:
+#     print("비어 있는 리스트")
