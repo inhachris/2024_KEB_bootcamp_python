34889b0097ede66bff5e305d646b3b4e7bbc7dc5
chris <chris4364@inha.edu>
2024-01-16 11:14:20 +0900

    v1.7 python day03

--- day03.py
+++ day03.py
@@ -1,18 +1,13 @@
-university = "Inha\nUniversity!"
-#university = r"Inha\nUniversity!"    # raw string
-#print(university)
+subjects = ["Python", "C++", "Database"]
+subjects_string = " / ".join(subjects)
+print(subjects_string)
 
-# slicing
-# print(university[:4])
-# print(university[:-11])
-# print(len(university))
-# print(university[0:len(university)])
-# print(university[:16])
-# print(university[::2])
+numbers = input("FirstNumber SecondNumber : ").split()
+#print(numbers[0] + numbers[1])    # concatenation
+print(int(numbers[0]) + int(numbers[1]))    # arithmetic operation
 
-number1 = input("First number : ")
-number2 = input("Second number : ")
-print(number1 + number2)    # concatenation
-print(number1 * 3)    # duplicate
-#print(number1 + 3)    # TypeError
-
+# course = "2024 KEB Bootcamp"
+# print(course)
+# # list_course = course.split()
+# list_course = course.split('B')
+# print(list_course)
