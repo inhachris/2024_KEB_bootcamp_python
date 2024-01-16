a66f58c9a5685f33c14a472b63f3aff32e5ffc27
chris <chris4364@inha.edu>
2024-01-16 14:59:30 +0900

    v2.5 python day03

--- day03.py
+++ day03.py
@@ -1,19 +1,17 @@
-# prime number
-number = int(input("Input number : "))
-is_prime = True
+univ = "inha"
+i = 0
+while i < len(univ):
+    print(univ[i], end=' ')
+    i = i + 1
 
-if number < 2:
-    print(f'{number} is NOT prime number!')
-else:
-    i = 2
+print()
 
-    while i < number:
-        if number % i == 0:
-            is_prime = False
-            break
-        i = i + 1
+for letter in univ:
+    print(letter, end=' ')
 
-    if is_prime:
-        print(f'{number} is prime number')
-    else:
-        print(f'{number} is NOT prime number!')
+print()
+
+# for k in range(0, len(univ), 1):
+# for k in range(0, len(univ)):
+for k in range(len(univ)):
+    print(univ[k], end=' ')
