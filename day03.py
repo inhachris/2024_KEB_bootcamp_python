b485328f918ae7d76f0ba3f6d9ccd7355cc0712b
chris <chris4364@inha.edu>
2024-01-16 10:34:50 +0900

    v1.6 python day03

--- day03.py
+++ day03.py
@@ -1,16 +1,18 @@
-# Assignment
-# (100°F - 32) × 5/9 = 37.778°C
-# (0°C × 9/5) + 32 = 32°F
+university = "Inha\nUniversity!"
+#university = r"Inha\nUniversity!"    # raw string
+#print(university)
 
-while True:
-    menu = input("1) Fahrenheit -> Celsius     2) Celsius -> Fahrenheit     3) Quit Program : ")
+# slicing
+# print(university[:4])
+# print(university[:-11])
+# print(len(university))
+# print(university[0:len(university)])
+# print(university[:16])
+# print(university[::2])
 
-    if menu == '1':
-        fahrenheit = float(input('Input Fahrenheit : '))
-        print(f'Fahrenheit : {fahrenheit}°F   ->   Celsius : {((fahrenheit - 32.0) * 5.0 / 9.0):.4f}°C\n')
-    elif menu == '2':
-        celsius = float(input('Input Celcius : '))
-        print(f'Celsius : {celsius}°C   ->   Fahrenheit : {((celsius*9.0/5.0) + 32.0):.4f}°F\n')
-    elif menu == '3':
-        print("Terminate Program")
-        break
+number1 = input("First number : ")
+number2 = input("Second number : ")
+print(number1 + number2)    # concatenation
+print(number1 * 3)    # duplicate
+#print(number1 + 3)    # TypeError
+
