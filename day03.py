cb80fbb997a73b9dc495d150b3bc048631e733a5
chris <chris4364@inha.edu>
2024-01-16 14:06:01 +0900

    v2.1 python day03 prime number

--- day03.py
+++ day03.py
@@ -1,14 +1,14 @@
 # prime number
 number = int(input("Input number : "))
 cnt = 0
-i = 1
+i = 2
 
-while i <= number:
+while i < number:
     if number % i == 0:
         cnt = cnt + 1
     i = i + 1
 
-if cnt == 2:
+if cnt == 0:
     print(f'{number} is prime number')
 else:
     print(f'{number} is NOT prime number!')
