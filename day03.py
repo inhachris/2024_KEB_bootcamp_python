53076eec7856433be4be485d8e3fc8455612802d
chris <chris4364@inha.edu>
2024-01-16 14:03:26 +0900

    v2.0 python day03 prime number

--- day03.py
+++ day03.py
@@ -1,16 +1,23 @@
-# preview
-subjects = "python c++ database linux"
-subject = input("수강신청과목 입력 : ")
+# prime number
+number = int(input("Input number : "))
+cnt = 0
+i = 1
 
-try:
-    print(f"해당 과목이 존재합니다. 위치는 {subjects.index(subject)}번 인덱스입니다.")
-except ValueError:
-    print('해당 과목이 존재하지 않습니다.')
+while i <= number:
+    if number % i == 0:
+        cnt = cnt + 1
+    i = i + 1
 
-# subjects = "python c++ database linux"
-# subject = input("수강신청과목 입력 : ")
-#
-# if subjects.find(subject) != -1:
-#     print(f'해당 과목이 존재합니다. 위치는 {subjects.find(subject)}번 인덱스입니다.')
-# else:
-#     print('해당 과목이 존재하지 않습니다.')
+if cnt == 2:
+    print(f'{number} is prime number')
+else:
+    print(f'{number} is NOT prime number!')
+
+
+
+
+
+
+
+# subjects = {'python': 'kim', 'c++': 'sung', 'data structure': 'kim', 'database': 'kang'}
+# print("{0[python]} {0[database]}".format(subjects))
