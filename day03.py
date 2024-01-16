2b139981e9b8fd9bdb2de5b60457c8d6b91668e7
chris <chris4364@inha.edu>
2024-01-16 15:42:01 +0900

    v2.8 python day03 prime number section

--- day03.py
+++ day03.py
@@ -3,6 +3,9 @@ numbers = input("Input first second number : ").split()
 n1 = int(numbers[0])
 n2 = int(numbers[1])
 
+if n1 > n2:
+    n1, n2 = n2, n1
+
 for number in range(n1, n2+1):
     is_prime = True
 
