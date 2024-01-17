39efaf86cc0f0b9757553fe67ccb183dfe884ab8
chris <chris4364@inha.edu>
2024-01-17 13:10:00 +0900

    v3.5 python day04

--- day04.py
+++ day04.py
@@ -1,24 +1,13 @@
-# prime number
-numbers = input("Input first second number : ").split()
-n1 = int(numbers[0])
-n2 = int(numbers[1])
-
-if n1 > n2:
-    n1, n2 = n2, n1
-
-for number in range(n1, n2+1):
-    is_prime = True
-
-    if number < 2:
-        continue  #pass
-    else:
-        i = 2
-        while i*i <= number:  # performance issue
-            if number % i == 0:
-                is_prime = False
-                break
-            i = i + 1
-            #print(i, end=' ')  # loop count
-        if is_prime:
-            pass
-            print(number, end=' ')
+subjects = ["데이터베이스", "C++", "5", "Java", "Python", "Java", "9", "리눅스"]
+#print(subjects[::-1])
+# subjects[::-1]
+#subjects = subjects[::-1] # subjects.reverse()
+print(subjects)
+# subjects.remove("Java")
+# del subjects[-2]
+# del subjects[2]
+#subjects.pop(2)
+#subjects.sort(reverse=True)    # desc
+copy_subjects = sorted(subjects)
+print(subjects)
+print(copy_subjects)
