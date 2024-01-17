fb87a74d1d4581b1cfbf9430af5aa1e1d5e931a6
chris <chris4364@inha.edu>
2024-01-17 18:12:35 +0900

    python day04 assignment 04 (Exercise 8.4 p.205)

--- day04.py
+++ day04.py
@@ -1,6 +1,7 @@
-# Assignment Exercise 8.3 (p.205)
+# Assignment Exercise 8.4 (p.205)
 
 e2f = {"dog": "chien", "cat": "chat", "walrus": "morse"}
-
 f2e = {value: key for key, value in e2f.items()}
+
 print(f2e)
+print(f2e['chien'])
