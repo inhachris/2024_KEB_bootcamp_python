0998bde82435280afc656490e2ef54a4d0fc5c23
chris <chris4364@inha.edu>
2024-01-17 14:18:49 +0900

    v3.7 python day04

--- day04.py
+++ day04.py
@@ -1,11 +1,19 @@
-import copy
-#subjects = ["a", "b", "c"]
-subjects = ["a", ["b", "c"], "d"]
-a = subjects
-b = subjects.copy()
-c = list(subjects)
-d = subjects[:]
-e = copy.deepcopy(a)
-print(subjects, a, b, c, d, e)
-subjects[1][1] = "x"
-print(subjects, a, b, c, d, e)
+# squares = list()
+# squares.append(1*1)
+# squares.append(2*2)
+# squares.append(3*3)
+# squares.append(4*4)
+# squares.append(5*5)
+# print(squares)
+
+# squares = list()
+# for i in range(1, 6, 1):
+#     squares.append(i*i)
+# print(squares)
+
+# list comprehension
+# squares = [i*i for i in range(1, 6, 1)]
+# print(squares)
+
+even_squares = [i*i for i in range(1, 6, 1) if i % 2 == 0]
+print(even_squares)
