85377dda0453f0e801266cd3b4e82db7be0a5286
chris <chris4364@inha.edu>
2024-01-18 10:53:40 +0900

    v4.2 prime number function day05

--- day05.py
+++ day05.py
@@ -1,16 +1,28 @@
-# Assignment ex 8.10
-# squares = {n: pow(n, 2) for n in range(10)}
-# squares = {n: n**2 for n in range(10)}
-squares = {n: n*n for n in range(10)}
-print(squares)
+# prime number
+def isprime(n) -> bool:
+    """
+    매개변수로 넘겨 받은 수가 소수인지 여부를 boolean으로 리턴
+    :param n: 판정할 매개변수
+    :return: 소수면 True, 소수가 아니면 False
+    """
+    if n < 2:
+        return False
+    else:
+        i = 2
+        while i*i <= n:
+            if n % i == 0:
+                return False
+            i += 1
+        return True
 
 
-# univ = 'inha university'
-# counts_alphabet = {letter: univ.count(letter) for letter in univ}
-# print(counts_alphabet)
+numbers = input("Input first second number : ").split()
+n1 = int(numbers[0])
+n2 = int(numbers[1])
 
-# univ = 'inha university'
-# counts_alphabet = dict()
-# for letter in univ:
-#     counts_alphabet[letter] = univ.count(letter)
-# print(counts_alphabet)
+if n1 > n2:
+    n1, n2 = n2, n1
+
+for number in range(n1, n2+1):
+    if isprime(number):
+        print(number, end=' ')
