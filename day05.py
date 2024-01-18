8885f9221fb810c28fafa08dc67ec20f8467eb80
chris <chris4364@inha.edu>
2024-01-18 09:25:24 +0900

    v4.1 assignment day05 (exercise 8.10)

---
+++ day05.py
@@ -0,0 +1,16 @@
+# Assignment ex 8.10
+# squares = {n: pow(n, 2) for n in range(10)}
+# squares = {n: n**2 for n in range(10)}
+squares = {n: n*n for n in range(10)}
+print(squares)
+
+
+# univ = 'inha university'
+# counts_alphabet = {letter: univ.count(letter) for letter in univ}
+# print(counts_alphabet)
+
+# univ = 'inha university'
+# counts_alphabet = dict()
+# for letter in univ:
+#     counts_alphabet[letter] = univ.count(letter)
+# print(counts_alphabet)
