f780ea5400ab69fc3bf440b27e10df32063467ac
chris <chris4364@inha.edu>
2024-01-18 13:36:41 +0900

    v4.4 python day05

--- day05.py
+++ day05.py
@@ -1,52 +1,24 @@
-# prime number with function
-def isprime(n) -> bool:
+# def squares(n):
+#     return n * n
+#
+# def run_function(f, number):
+#     return f(number)
+
+# print(squares(7))
+# print(run_function(squares, 9))
+
+def squares(*n) -> list:
     """
-    매개변수로 넘겨 받은 수가 소수인지 여부를 boolean으로 리턴
-    :param n: 판정할 매개변수
-    :return: 소수면 True, 소수가 아니면 False
+    넘겨 받은 수치 데이터들의 거듭제곱 값을 리스트에 담아서 리턴
+    :param n: tuple
+    :return: list
     """
-    if n < 2:
-        return False
-    else:
-        i = 2
-        while i*i <= n:
-            if n % i == 0:
-                return False
-            i += 1
-        return True
+    return [i*i for i in n]
+    # return n * n
 
-while True:
-    menu = input("1) Fahrenheit -> Celsius     2) Celsius -> Fahrenheit \
-    3) Deciding Prime Number     4) Prime Numbers In a Section     5) Quit Program : ")
+def run_function(f, *number) -> list:
+    return f(*number)
 
-    if menu == '1':
-        fahrenheit = float(input('Input Fahrenheit : '))
-        print(f'Fahrenheit : {fahrenheit}°F   ->   Celsius : {((fahrenheit - 32.0) * 5.0 / 9.0):.4f}°C\n')
 
-    elif menu == '2':
-        celsius = float(input('Input Celcius : '))
-        print(f'Celsius : {celsius}°C   ->   Fahrenheit : {((celsius*9.0/5.0) + 32.0):.4f}°F\n')
-
-    elif menu == '3':
-        number = int(input("Input number : "))
-        if isprime(number):
-            print(f'{number} is prime number\n')
-        else:
-            print(f'{number} is NOT prime number\n')
-
-    elif menu == '4':
-        numbers = input("Input first second number : ").split()
-        n1 = int(numbers[0])
-        n2 = int(numbers[1])
-
-        if n1 > n2:
-            n1, n2 = n2, n1
-
-        for number in range(n1, n2 + 1):
-            if isprime(number):
-                print(number, end=' ')
-        print("\n")
-
-    elif menu == '5':
-        print("Terminate Program")
-        break
+print(squares(7, 5, 2))
+print(run_function(squares, 9, 10))
