c693c882130d21f260fae8e217537b13f23960a1
chris <chris4364@inha.edu>
2024-01-18 18:21:36 +0900

    python day05 assignment 01 (Exercise 9.1 p.242)

--- day05.py
+++ day05.py
@@ -1,50 +1,7 @@
-# decorator
-def description(f):  # closure
-    def inner(*args):
-        print(f.__name__)
-        print(f.__doc__)
-        r = f(*args)
-        return r
+# Assignment Exercise 9.1 (p.242)
 
-    return inner
+def good():
+    return ['Harry', 'Ron', 'Hermione']
 
-
-def squares(n):
-    """
-    제곱 함수
-    """
-    return n * n
-
-@description
-def power(b, e):
-    """
-    거듭제곱 함수
-    """
-    result = 1
-    for _ in range(e):
-        result = result * b
-    return result
-
-
-f1 = description(squares)
-print(f1(9))
-print(power(2, 10))
-# f2 = description(power)
-# print(f2(2, 10))
-
-# print(squares(7))
-# print(squares.__doc__)
-
-# def my_range(first=0, last=5, step=1):
-#     number = first
-#     while number < last:
-#         yield number
-#         number += step
-#
-# r = my_range()
-# print(r, type(r))
-#
-# for x in r:
-#     print(x)
-# for x in r:
-#     print(x)
+a = good()
+print(a)
