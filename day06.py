4fa06d1163703b988c2bd34089b0b2c4d70eb757
chris <chris4364@inha.edu>
2024-01-19 15:25:17 +0900

    v6.4 python day06 (Class_property)

--- day06.py
+++ day06.py
@@ -1,23 +1,25 @@
 class FlyingMixin:
     def fly(self):
-        return f"{self.name}이(가) 하늘을 훨훨 날아갑니다~"
+        return f"{self.hidden_name}이(가) 하늘을 훨훨 날아갑니다~"
 
 class SwimmingMixin:
     def swim(self):
-        return f"{self.name}이(가) 수영을 합니다."
+        return f"{self.hidden_name}이(가) 수영을 합니다."
 
 class Pokemon:
     def __init__(self, name):
-        self.name = name
+        self.hidden_name = name
 
     def attack(self):    # self는 반드시 객체 (클래스x)
         print("공격~")
 
     def get_name(self):
-        return self.name
+        return self.hidden_name
 
     def set_name(self, new_name):
-        self.name = new_name
+        self.hidden_name = new_name
+
+    name = property(get_name, set_name)
 
 class Charizard(Pokemon, FlyingMixin):
     pass
@@ -25,13 +27,15 @@ class Charizard(Pokemon, FlyingMixin):
 class Gyarados(Pokemon, SwimmingMixin):
     pass
 
+
 g1 = Gyarados("갸라도스")
 c1 = Charizard("리자몽")
-# print(c1.fly())
-# print(g1.swim())
-# c1.attack()
-# # Charizard.attack()    # TypeError    c1, g1과 같은 객체가 self
-# Charizard.attack(c1)
-print(g1.get_name())
-g1.set_name("잉어킹")
-print(g1.get_name())
+
+# print(g1.get_name())
+# g1.set_name("잉어킹")
+# print(g1.get_name())
+
+# property
+print(g1.name)
+g1.name = "잉어킹"
+print(g1.name)
