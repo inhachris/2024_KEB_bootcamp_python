2affd07db7c75b45468f4b708885c7581c7a2461
chris <chris4364@inha.edu>
2024-01-19 09:16:37 +0900

    v5.1 assignment 9.2 day06

--- day05.py
+++ day05.py
@@ -1,9 +1,16 @@
-def good() -> list:
+def get_odds(n) -> int:
     """
-    chapter 9 things to do. 91.
-    :return: list
+    1부터 n까지의 홀수를 발생시키는 제네레이터
+    :param n: int
+    :return: int
     """
-    harry_porter = input().split()
-    return harry_porter
+    for i in range(1, n+1, 2):
+        yield i
 
-print(good())
+cnt = 0
+odds = get_odds(9)
+for odd in odds:
+    cnt += 1
+    if cnt == 3:
+        print(f'Third number is {odd}')
+        break
