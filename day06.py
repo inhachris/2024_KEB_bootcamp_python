996b51674494dfd6245c962a051b3ab8cd782f17
chris <chris4364@inha.edu>
2024-01-19 15:17:59 +0900

    v6.2 python day06 (Class_direct change)

--- day06.py
+++ day06.py
@@ -10,6 +10,9 @@ class Pokemon:
     def __init__(self, name):
         self.name = name
 
+    def attack(self):    # self는 반드시 객체 (클래스x)
+        print("공격~")
+
 class Charizard(Pokemon, FlyingMixin):
     pass
 
@@ -18,5 +21,11 @@ class Gyarados(Pokemon, SwimmingMixin):
 
 g1 = Gyarados("갸라도스")
 c1 = Charizard("리자몽")
-print(c1.fly())
-print(g1.swim())
+# print(c1.fly())
+# print(g1.swim())
+# c1.attack()
+# # Charizard.attack()    # TypeError    c1, g1과 같은 객체가 self
+# Charizard.attack(c1)
+print(g1.name)
+g1.name = "잉어킹"
+print(g1.name)    # 편하고 좋지만 안전하지 않음
