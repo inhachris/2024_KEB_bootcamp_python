16f27de2f8632050fd7cb02f4f9e56017b587fb1
chris <chris4364@inha.edu>
2024-01-19 10:54:46 +0900

    v5.5 python day06 (Exception)

--- day06.py
+++ day06.py
@@ -1,27 +1,20 @@
-def factorial_repetition(n) -> int:
-    """
-    반복문을 이용한 팩토리얼 함수
-    :param n: 정수, int
-    :return: 팩토리얼 값, int
-    """
-    result = 1
-    for i in range(2, n+1):
-        result = result * i
-    return result
+import random
 
-def factorial_recursion(n):
-    """
-    재귀함수를 사용한 팩토리얼 함수
-    :param n: 정수, int
-    :return: function, int
-    """
-    if n == 1:
-        return n
-    else:
-        return n * factorial_recursion(n-1)
+# numbers = list()
+# for i in range(5):
+#     numbers.append(random.randint(1, 100))
+numbers = [random.randint(1, 100) for i in range(10)]
+print(numbers)
 
-# print(factorial_repetition(int(input("number : "))))
-# print(factorial_recursion(int(input("number : "))))
-number = int(input("number : "))
-print(factorial_repetition(number))
-print(factorial_recursion(number))
+try:
+    pick = int(input(f"Input index (0 ~ {len(numbers)-1}) : "))
+    print(numbers[pick])
+    print(5/0)
+except IndexError as err:
+    print(f"Wrong index number\n{err}")
+except ValueError as err:
+    print(f"Input Only Number\n{err}")
+except ZeroDivisionError as err:
+    print(f"Denominator cannot be zero\n{err}")
+except Exception as err:    # Exception 맨 밑에!
+    print(f"Error occurs : {err}")
