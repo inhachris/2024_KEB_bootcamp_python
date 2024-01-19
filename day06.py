dcea91dc6534187cb6be5d54d6267b4280294b33
chris <chris4364@inha.edu>
2024-01-19 14:46:51 +0900

    v6.0 python day06 (Class_Subclass)

--- day06.py
+++ day06.py
@@ -1,30 +1,36 @@
-# class Pokemon:
-#     pass
-#
-# pikachu = Pokemon()
-# squirtle = Pokemon()
-# pikachu.name = "피카츄"
-# pikachu.nemesis = squirtle
-# print(pikachu.name)
-# #print(pikachu.nemesis.name)
-# #squirtle.name = "꼬부기"
-# pikachu.nemesis.name = "꼬부기" #squirtle.name = "꼬부기"
-# print(pikachu.nemesis.name)
-# print(squirtle.name)
-
-# class Pokemon():
 class Pokemon:
     def __init__(self, name):
         self.name = name
-        print(f"{name} 포켓몬스터 생성")
 
     def attack(self, target):
         print(f"{self.name}이(가) {target.name}을(를) 공격!")
 
+#class Pikachu:
+class Pikachu(Pokemon):  # is-a
+    def __init__(self, name, type):
+        super().__init__(name)
+        self.type = type
 
-charizard = Pokemon("리자몽")
-pikachu = Pokemon("피카츄")
-squirtle = Pokemon("꼬부기")
-charizard.attack(squirtle)
-# print(pikachu.name)
-# print(squirtle.name)
+    def attack(self, target):
+        print(f"{self.name}이(가) {target.name}을(를) {self.type} 공격!")
+
+    def electric_info(self):
+        print("전기 계열의 공격을 합니다")
+
+
+class Squirtle(Pokemon):  # is-a
+    pass
+
+class Agumon:
+    pass
+
+p1 = Pikachu("피카츄", "전기")
+p2 = Squirtle("꼬부기")
+p3 = Pokemon("아무개")
+p1.electric_info()
+#p3.electric_info()  # AttributeError: 'Pokemon' object has no attribute 'electric_info'
+p1.attack(p2)
+p2.attack(p1)
+print(p1.name, p1.type)
+print(issubclass(Pikachu, Pokemon))
+print(issubclass(Agumon, Pokemon))
