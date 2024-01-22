ebcedc5d1b36c5355e6c06f5a68c0cd1996a8ac3
chris <chris4364@inha.edu>
2024-01-22 11:37:10 +0900

    v7.3 python day07

--- day07.py
+++ day07.py
@@ -20,12 +20,11 @@ class FlyWithWings(FlyingBehavior):
 
 
 class Pikachu:
-    def __init__(self, name, hp, fly):
+    def __init__(self, name, hp):
         self.name = name
         self.hp = hp
-        self.fly_behavior = fly  # aggregation
+        self.fly_behavior = NoFly()  # composition
 
 
-nofly = NoFly()
-p1 = Pikachu("피카츄", 35, nofly)
+p1 = Pikachu("피카츄", 35)
 print(p1.fly_behavior.fly())
