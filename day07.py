199ca4b8f36ab2bfc59cfec131ed947b8550e4e0
chris <chris4364@inha.edu>
2024-01-22 13:35:03 +0900

    v8.0 python day07

--- day07.py
+++ day07.py
@@ -1,5 +1,6 @@
 # module
-import mymath as mm
+# import mymath as mm
+from mymath import isprime, fahrenheit_to_celsius, celsius_to_fahrenheit
 
 #print(globals())
 
@@ -9,13 +10,13 @@ if __name__ == "__main__":
 
         if menu == '1':
             fahrenheit = float(input('Input Fahrenheit : '))
-            print(f'Fahrenheit : {fahrenheit}F, Celsius : {mm.fahrenheit_to_celsius(fahrenheit):.4f}C')
+            print(f'Fahrenheit : {fahrenheit}F, Celsius : {fahrenheit_to_celsius(fahrenheit):.4f}C')
         elif menu == '2':
             celsius = float(input('Input Celsius : '))
-            print(f'Celsius : {celsius}C, Fahrenheit : {mm.celsius_to_fahrenheit(celsius):.4f}F')
+            print(f'Celsius : {celsius}C, Fahrenheit : {celsius_to_fahrenheit(celsius):.4f}F')
         elif menu == '3':
             number = int(input("Input number : "))
-            if mm.isprime(number):
+            if isprime(number):
                 print(f'{number} is prime number')
             else:
                 print(f'{number} is NOT prime number!')
@@ -28,7 +29,7 @@ if __name__ == "__main__":
                 n1, n2 = n2, n1
 
             for number in range(n1, n2 + 1):
-                if mm.isprime(number):
+                if isprime(number):
                     print(number, end=' ')
             print()
         elif menu == '5':
