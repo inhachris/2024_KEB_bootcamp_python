4ab0efd8f11325c6aaf92168bb68fce90969e83d
chris <chris4364@inha.edu>
2024-01-22 14:01:01 +0900

    Assignment Pokemon v1.1

--- Assignment_Pokemon.py
+++ Assignment_Pokemon.py
@@ -4,10 +4,14 @@ class Pokemon:
     def __init__(self, name):
         self.name = name
 
+    def attack(self, target):
+        print(f"{self.name}가 {target.name}를 공격!")
+
 class Charmander(Pokemon):
     def __init__(self, name, type):
         super().__init__(name)
         self.type = type
+
 class Squirtle(Pokemon):
     def __init__(self, name, type):
         super().__init__(name)
@@ -35,7 +39,8 @@ while True:
         if user_pokemon == 1:
             a = input(f"정말로 {c1.name}를 선택하시겠습니까? Y / N : ")
             if a == 'Y':
-                print(f"\n당신의 포켓몬은 {c1.name}입니다. {c1.name}는 {c1.type}타입 포켓몬입니다.")
+                print(f"\n당신의 포켓몬은 {c1.name}입니다. {c1.name}는 {c1.type}타입 포켓몬입니다.\n")
+                starting_pokemon = Charmander("파이리", "불꽃")
                 break
             elif a == 'N':
                 print()
@@ -45,7 +50,8 @@ while True:
         elif user_pokemon == 2:
             a = input(f"정말로 {s1.name}를 선택하시겠습니까? Y / N : ")
             if a == 'Y':
-                print(f"\n당신의 포켓몬은 {s1.name}입니다. {s1.name}는 {s1.type}타입 포켓몬입니다.")
+                print(f"\n당신의 포켓몬은 {s1.name}입니다. {s1.name}는 {s1.type}타입 포켓몬입니다.\n")
+                starting_pokemon = Squirtle("꼬부기", "물")
                 break
             elif a == 'N':
                 print()
@@ -55,7 +61,8 @@ while True:
         elif user_pokemon == 3:
             a = input(f"정말로 {b1.name}를 선택하시겠습니까? Y / N : ")
             if a == 'Y':
-                print(f"\n당신의 포켓몬은 {b1.name}입니다. {b1.name}는 {b1.type}타입 포켓몬입니다.")
+                print(f"\n당신의 포켓몬은 {b1.name}입니다. {b1.name}는 {b1.type}타입 포켓몬입니다.\n")
+                starting_pokemon = Bulbasaur("이상해씨", "풀")
                 break
             elif a == 'N':
                 print()
@@ -65,7 +72,8 @@ while True:
         elif user_pokemon == 4:
             a = input(f"정말로 {p1.name}를 선택하시겠습니까? Y / N : ")
             if a == 'Y':
-                print(f"\n당신의 포켓몬은 {p1.name}입니다. {p1.name}는 {p1.type}타입 포켓몬입니다.")
+                print(f"\n당신의 포켓몬은 {p1.name}입니다. {p1.name}는 {p1.type}타입 포켓몬입니다.\n")
+                starting_pokemon = Pikachu("피카츄", "전기")
                 break
             elif a == 'N':
                 print()
@@ -75,5 +83,19 @@ while True:
         else:
             print("다시 입력해주세요\n")
 
+    except ValueError:
+        print("다시 입력해주세요\n")
+
+while True:
+    try:
+        behavior = int(input(f"야생의 {s1.name}를 만났다!\n1)공격  2)도망  3)게임종료 : "))
+        if behavior == 1:
+            print(f"{starting_pokemon.name}가 {c1.name}를 {starting_pokemon.type} 공격!\n")
+        elif behavior == 2:
+            print(f"{starting_pokemon.name}와 무사히 도망쳤다\n")
+            break
+        elif behavior == 3:
+            print(f"게임을 종료합니다\n")
+            break
     except ValueError:
         print("다시 입력해주세요\n")
